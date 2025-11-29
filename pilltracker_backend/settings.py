3d622bbe5f0021c138bf0bce0064c8276f7c5cb2
Mansi6183 <22012035@pvgcoet.ac.in>
2025-11-29 12:46:44 +0530

    Added Render domain to ALLOWED_HOSTS

--- pilltracker_backend/settings.py
+++ pilltracker_backend/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-15$*$$u_qmwxczof5ajw)l3bss^#%%1co4)1&2$*ffr+=#&a3r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['smartpillbox.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
